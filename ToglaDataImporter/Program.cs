ff378f52039e026ded0d8d27d58c44ab6ad6642e
Miles Vendetti <milesvendetti@gmail.com>
2020-11-08 20:11:51 -0500

    Adds files for data importer console project, this is a WIP

---
+++ ToglaDataImporter/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Togla.Data;
+using Togla.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ToglaDataImporter
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			DbContextOptions options
+
+			///ToglaDbContext db = new ToglaDbContext(new Microsoft.EntityFrameworkCore.DbContextOptions());
+
+			List<Stock> stocksToImport = new List<Stock>();
+
+			stocksToImport.Add(new Stock
+			{
+				SymbolTicker = "MSFT",
+				Price = 151.50m,
+
+			});
+			
+
+			
+			// Open up a CSV file.
+
+			// for every row in the CSV,
+
+			// Create Stock object
+
+			// Pass into database and save
+
+
+
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
