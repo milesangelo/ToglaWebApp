442b795e276c9a0c48b47db8b261bea9db58ff16
milesangelo <milesvendetti@gmail.com>
2020-11-27 21:23:08 -0700

    Updates Data project - net 5, updates Stock model, resets migrations

--- Togla.Data/ToglaDbContext.cs
+++ Togla.Data/ToglaDbContext.cs
@@ -8,11 +8,31 @@ namespace Togla.Data
 {
 	public class ToglaDbContext: DbContext
 	{
+		/// <summary>
+		/// 
+		/// </summary>
 		public ToglaDbContext() { }
 
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="options"></param>
 		public ToglaDbContext(DbContextOptions options) : base(options) { }
 
+		/// <summary>
+		/// 
+		/// </summary>
 		public virtual DbSet<Stock> Stocks { get; set; }
 
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="optionsBuilder"></param>
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseNpgsql(
+				"Host=localhost;Port=5432;Username=postgres;Password=GNUsn0tunix;Database=togla.dev2;");
+			base.OnConfiguring(optionsBuilder);
+		}
 	}
 }
